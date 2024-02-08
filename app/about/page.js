b6b5f8cbcd0456e472489798b92e84dc35c87080
stoilstoychev <stoilstoychev@gmail.com>
2024-02-08 14:28:30 +0200

    Add /about page with simple description

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: `Stoilsky: About`,
+    description: 'About Stoil Stoychev, a software engineer and author of Stoilsky.com'
+}
+export default async function PostPage({ params }) {
+    return (
+        <div>
+            Hi, I'm Stoil Stoychev, a JavaScript enthusiast and Software Engineer.
+            <br/><br/>
+            Mostly on the Frontend side of things, but I enjoy dabbling elsewhere.
+            <br/><br/>
+            JavaScript, TypeScript, React, State Managers, Responsive Design: these are some of the things I know well.
+        </div>
+    );
+}
