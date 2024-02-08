b9856d72c7c18182e592f71e98d2f7d3a8d84650
stoilstoychev <stoilstoychev@gmail.com>
2024-02-08 14:29:45 +0200

    Add Footer to layout
    - Add © with current year
    - Link to overreacted.io to acknowledge fork

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,37 @@
 import Link from "./Link";
 import HomeLink from "./HomeLink";
 import AutoRefresh from "./AutoRefresh";
-import { serif } from "./fonts";
+import {serif} from "./fonts";
 import "./global.css";
 
 export default function RootLayout({ children }) {
   return (
     <AutoRefresh>
       <html lang="en" className={serif.className}>
-        <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
-          <header className="mb-14 flex flex-row place-content-between">
-            <HomeLink />
-            <span className="relative top-[4px] italic">
-              by{" "}
-              <Link href="https://uk.linkedin.com/in/stoil-stoychev-9008aa93" target="_blank">
+      <body className="mx-auto max-w-2xl bg-[--bg] px-5 pt-12 pb-2 text-[--text]">
+      <header className="mb-14 flex flex-row place-content-between">
+        <HomeLink/>
+        <span className="relative top-[4px] italic">
+              <Link href="/about" target="_blank">
+                  Author{" "}
                 <img
-                  alt="Stoil Stoychev"
-                  src="https://github.com/stoich.png"
-                  className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
+                    alt="Stoil Stoychev"
+                    src="https://github.com/stoich.png"
+                    className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
                 />
               </Link>
             </span>
-          </header>
-          <main>{children}</main>
-        </body>
+      </header>
+      <main>{children}</main>
+      <div className={"mt-10 flex flex-row place-content-center"}>
+        <span className={"text-xs font-light font-sans"}>
+            © Stoilsky {new Date().getFullYear()} | Fork of{" "}
+            <Link className={"underline"} href="https://github.com/gaearon/overreacted.io" target="_blank">
+                overreacted.io
+            </Link>
+        </span>
+      </div>
+      </body>
       </html>
     </AutoRefresh>
   );
