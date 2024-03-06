a23ce50cd55b9432ffe96925feacc8e885a2b964
stoilstoychev <stoilstoychev@gmail.com>
2024-03-06 11:30:11 +0200

    Add Product Hunt banner

---
+++ app/ProductHuntBanner.js
@@ -0,0 +1,23 @@
+
+
+const Banner = () => {
+    return <div>
+        <a
+            href="https://www.producthunt.com/posts/qajobs-co-quality-assurance-jobs?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-qajobs&#0045;co&#0045;quality&#0045;assurance&#0045;jobs"
+            target="_blank"
+        >
+            <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=442882&theme=light"
+                alt="QAJobs&#0046;co&#0032;&#0124;&#0032;Quality&#0032;Assurance&#0032;Jobs - The&#0032;premier&#0032;job&#0032;board&#0032;for&#0032;QA&#0032;and&#0032;Test&#0032;Automation&#0032;pros | Product Hunt"
+                style={{
+                    width: '250px',
+                    height: '54px',
+                }}
+                width="250"
+                height="54"
+            />
+        </a>
+    </div>
+}
+
+export default Banner;
--- app/QAJobsCTA.js
+++ app/QAJobsCTA.js
@@ -1,9 +1,13 @@
+import ProductHuntBanner from './ProductHuntBanner'
+
 export const QAJobsCTA = () => (
     <div className="bg-blue-200 text-gray-800 pt-6 pb-6 px-6 rounded-lg flex flex-col items-center justify-center space-y-2">
         <h1 className="font-bold text-3xl text-center mb-4">
             Join Quality Assurance Jobs!
         </h1>
-        <div className="text-center pb-5">
+
+        <ProductHuntBanner/>
+        <div className="text-center pb-5 pt-3">
             <a className={`underline`} href={`https://www.qajobs.co/`}>qajobs.co</a> is the first job board for Quality Assurance, SDET, and all other Test
             Automation Engineer roles.
         </div>
